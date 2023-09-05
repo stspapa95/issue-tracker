b70f0408fb13c1d08a35cf755a0a2161756353a1
stspapa95 <sts.papa@gmail.com>
2023-09-05 18:54:37 +0300

    Initialize React project, setup service MSW, installed basic deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { worker } from "@uidotdev/react-query-api";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const queryClient = new QueryClient();
+new Promise((res) => setTimeout(res, 100))
+  .then(() =>
+    worker.start({
+      quiet: true,
+      onUnhandledRequest: "bypass",
+    }),
+  )
+  .then(() => {
+    const root = ReactDOM.createRoot(
+      document.getElementById("root") as HTMLElement,
+    );
+    root.render(
+      <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </React.StrictMode>,
+    );
+  });
